78bac6921e7244b04117e4db7c5cc901a1048eed
pranavkalpathishankar <pranavkalpathishankar@outlook.com>
2026-04-20 13:56:00 -0500

    Adding Routing

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,20 @@
-﻿namespace PawsitivePlace
+﻿using PawsitivePlace.View;
+
+namespace PawsitivePlace
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            RegisterRoutes();
+        }
+
+        private void RegisterRoutes()
+        {
+            // Layouts
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+
         }
     }
 }
