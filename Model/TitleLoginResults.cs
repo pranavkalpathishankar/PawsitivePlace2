486003001e41b790e4ef4730ae8503fd74384b3c
pranavkalpathishankar <pranavkalpathishankar@outlook.com>
2026-04-22 13:30:48 -0500

    Changes to Models to Fix Errors

--- Model/TitleLoginResults.cs
+++ Model/TitleLoginResults.cs
@@ -1,11 +1,10 @@
-using Microsoft.Maui.Controls;
-
-namespace PawsitivePlace.Model;
-
-public class TitleLoginResults : ContentPage
+namespace PawsitivePlace.Model
 {
-	public TitleLoginResults()
+	public class TitleLoginResults
 	{
+		public TitleLoginResults()
+		{
 		
+		}
 	}
 }
