b7ce85b087c596c56d71e9630bdc998ab3d66322
Abhinavyerr <abhinav7nps@gmail.com>
2026-04-28 14:40:37 -0500

    abhinav

--- Model/Temp/HunterBrown.cs
+++ Model/Temp/HunterBrown.cs
@@ -2,4 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 
-PawsitivePlace.Model.Temp
+namespace PawsitivePlace.Model.Temp
+{
+    internal class HunterBrown
+    {
+       
+    }
+}
--- View/MainPage.xaml.cs
+++ View/MainPage.xaml.cs
@@ -7,5 +7,4 @@ namespace PawsitivePlace.View
                 InitializeComponent();
             }
         }
-    }
-}
+ }
