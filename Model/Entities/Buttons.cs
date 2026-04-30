732cb4cb15b17acf8af9feda5a4411204ec2b361
SamanyuCompSci <samanyushetty.us@gmail.com>
2026-04-30 14:42:45 -0500

    sam

---
+++ Model/Entities/Buttons.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PawsitivePlace.Model.Entities
+{
+    public class Buttons
+    {
+        public static ImageSource SubmitButton = ImageSource.FromFile("buttonsubmit.png");
+    }
+}
--- ViewModel/LoginPageViewModel.cs
+++ ViewModel/LoginPageViewModel.cs
@@ -1,26 +1,34 @@
-using PawsitivePlace.Model;
 using CommunityToolkit.Mvvm.ComponentModel;
-using PawsitivePlace.View;
 using CommunityToolkit.Mvvm.Input;
+using PawsitivePlace.Model;
+using PawsitivePlace.View;
+using PawsitivePlace.Model.Entities;
+namespace PawsitivePlace.ViewModel;
 
-namespace PawsitivePlace.ViewModel
+public partial class LoginPageViewModel : ObservableObject
 {
-    public partial class LoginPageViewModel : ObservableObject
+    [ObservableProperty]
+    private ImageSource submitButton = Buttons.SubmitButton;
+    public string Title => TitleLogin.Title;
+
+    [ObservableProperty]
+    private string userNameString = TitleLogin.UserNameString;
+
+    [ObservableProperty]
+    private string passwordString = TitleLogin.PasswordString;
+
+    public LoginPageViewModel()
     {
-        public string Title => TitleLogin.Title;
 
-        [ObservableProperty]
-        private string userNameString = TitleLogin.UserNameString;
+    }
 
-        [ObservableProperty]
-        private string passwordString = TitleLogin.PasswordString;
-
-        public LoginPageViewModel()
+    [RelayCommand]
+    private async Task SubmitClicked()
+    {
+        if(string.IsNullOrEmpty(userNameString) && string.IsNullOrEmpty(passwordString))
         {
-
+            await App.Current.MainPage.DisplayAlert("null!","Cannot be null","continue");
         }
-
-
-
+        await Shell.Current.GoToAsync(nameof(LoginResultsPage));
     }
 }
