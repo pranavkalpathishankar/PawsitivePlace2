281470b192a4e5e4afef57a48cdfb730d9c18069
SamanyuCompSci <samanyushetty.us@gmail.com>
2026-05-06 13:27:53 -0500

    sam

--- View/LoginPage.xaml.cs
+++ View/LoginPage.xaml.cs
@@ -8,4 +8,25 @@ public partial class LoginPage : ContentPage
 		InitializeComponent();
 		BindingContext = new LoginPageViewModel();
 	}
+
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        if (BindingContext is LoginPageViewModel viewModel)
+        {
+            // Get the list of usernames
+            if (query.TryGetValue("UserNameText", out var usernameValue) &&
+               usernameValue is string usernames)
+            {
+                viewModel.UserNameString = usernames;
+            }
+
+            // Get the single password
+            if (query.TryGetValue("PassText", out var passwordValue) &&
+               passwordValue is string password)
+            {
+                viewModel.PasswordString = password;
+            }
+        }
+    }
 }
+
